10dbfdf01a1e4185b956633d5ee89ad240f7c0dd
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-10 23:16:34 -0500

    new fetch function with book by ID

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,30 +1,66 @@
 //Basic Fetch by title or category
+const booksTitleAPI = 'https://www.googleapis.com/books/v1/volumes?q=title:';
+const booksByIdAPI = 'https://www.googleapis.com/books/v1/volumes/'
 
-let bookTitle = "The Hunger Games";
+const key = 'AIzaSyB-PoP7pVCAWxrn4HnA-Gwt6H8konn7WqI';
 
-function getBook() {
-    fetch('https://www.googleapis.com/books/v1/volumes?q=title:' + bookTitle)
+let categories = [];
 
-        .then((response) => response.json())
+let bookTitle = "fifty Shades of Grey";
 
-        .then((data) => console.log('Books by Title', data));
+const getBook = async () => {
+    let id;
+    await fetch( booksTitleAPI + bookTitle)
 
+        .then(function (response) {
+        return response.json();
+        })
+
+        .then(function (response) {
+            id = response.items[0].id;
+            console.log('Book ID: ', id);
+        });
+        bookURL = booksByIdAPI + id + "?key=" + key ;
+
+        const actualBook = async () => {
+            await fetch(bookURL)
+            .then(function (response) {
+                return response.json();
+                })
+            .then(function (response){
+                categories = response.volumeInfo.categories
+                console.log(categories);
+            })
+        }
+
+        actualBook();
 };
 
-let categories = "fiction";
 
-function getCategory() {
-     fetch('https://www.googleapis.com/books/v1/volumes?q=subject:' + categories)
 
-        .then((response) => response.json())
+// let categories = "fiction";
 
-        .then((data) => console.log('Books by Category', data));
+// function getCategory() {
+//      fetch('https://www.googleapis.com/books/v1/volumes?q=subject:' + categories)
+
+//         .then((response) => response.json())
+
+//         .then((data) => console.log('Books by Category', data));
                
-};
+// };
 
+// fetch(booksTitleAPI)
+// .then(function (response) {
+//     return response.json();
+//     })
+
+//     .then(function (response) {
+//         console.log(response);
+//     })
 
 getBook();
 
-getCategory();
+
+// getCategory();
 
 
