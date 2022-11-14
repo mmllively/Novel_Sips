3147c92ff6353ca4e4b9f60529cf02b3ede0d0c7
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-13 20:07:46 -0500

    working branch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -64,43 +64,44 @@ router.get('/drink/:id', async (req, res) => {
   }
 });
 
-// // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Drink }],
-//     });
+// Use withAuth middleware to prevent access to route
+// Need to wadd withAuth 
+router.get('/profile', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Drink }],
+    });
 
-//     const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-//     res.render('homepage', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('homepage', {
+      // ...user,
+      // logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/login', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/profile');
-//     return;
-//   }
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  // if (req.session.logged_in) {
+  //   res.redirect('/profile');
+  //   return;
+  // }
 
-//   res.render('login');
-// });
+  res.render('login');
+});
 
-// router.get('/signup', (req, res) => {
-//   if (req.session.signedUp) {
-//     res.redirect('/');
-//     return;
-//   }
-//   res.render('signup');
-// });
+router.get('/signup', (req, res) => {
+  // if (req.session.signedUp) {
+  //   res.redirect('/');
+  //   return;
+  // }
+  res.render('signup');
+});
 
 
 
