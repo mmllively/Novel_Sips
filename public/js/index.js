b440be0e0040dcbe0f2f7e4ac0c80841315a62ef
Karina Drummond <k.drummond528@gmail.com>
2022-11-14 21:18:33 -0800

    added contact us and about us handlebars pg add css styling to new handlebars pgs updated handlebars pgs

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
 // API URLS
 
-function getBooks(){
-    searchBtn = document.getElementById('output').innerHTML="";
-    
-    fetch("http://openlibrary.org/search.json?q=" +document.getElementById("input").value)
-    .then(a=>a.json())
-    .then(response =>{
-        for (let i = 0; i < 10; i++) {
-            document.getElementById("output").innerHTML+= "<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src = 'http://covers.openlibrary.org/b/isbn/" +response.docs[i].isbn[0]+"-M.jpg'><br>";     
-        }
-      
-    });
+function getBooks() {
+    searchBtn = document.getElementById('output').innerHTML = "";
+
+    fetch("http://openlibrary.org/search.json?q=" + document.getElementById("input").value)
+        .then(a => a.json())
+        .then(response => {
+            for (let i = 0; i < 10; i++) {
+                document.getElementById("output").innerHTML += "<h2>" + response.docs[i].title + "</h2>" + response.docs[i].author_name[0] + "<br><img src = 'http://covers.openlibrary.org/b/isbn/" + response.docs[i].isbn[0] + "-M.jpg'><br>";
+            }
+
+        });
 }
 
 document.getElementById("searchBtn").addEventListener("click", getBooks);
